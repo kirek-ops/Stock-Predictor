c199a088ae9aa8fe5d20a36be9beb68a725023fb
kirek <kirylmeleniako@gmail.com>
2023-11-18 23:51:26 +0100

    Prepared Data

--- main.py
+++ main.py
@@ -18,3 +18,20 @@ start = datetime(2012, 1, 1)
 end = datetime(2020, 1, 1)
 
 data = pdr.get_data_yahoo(company, start=start, end=end)
+
+# Prepare Data 
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x - prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
